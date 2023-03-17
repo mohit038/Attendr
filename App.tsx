2bfc9515d62ffa1ec541a4c9f5ef8dfa992a2837
mohit038 <112958629+mohit038@users.noreply.github.com>
2023-03-18 00:29:24 +0530

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeView from "./pages/HomeView";
+import * as React from "react";
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <HomeView />
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    secondary: "yellow",
   },
-});
+};
---
+++ pages/HomeView.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button } from "react-native-paper";
+
+const HomeView = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log("Pressed")}
+      >
+        Press me
+      </Button>
+    </View>
+  );
+};
+
+export default HomeView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
